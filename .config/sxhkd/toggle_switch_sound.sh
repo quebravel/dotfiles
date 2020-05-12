c34ca3ea5ec28a0ac34c95755a3bf4d94ae61218
quebravel <jonatasgb@gmail.com>
2020-05-11 22:42:59 -0300

    new script toogle sound in sxhkdrc

--- .config/sxhkd/toggle_switch_sound.sh
+++ .config/sxhkd/toggle_switch_sound.sh
@@ -1,93 +1,14 @@
-#!/bin/bash
+#!/bin/bash 
 
-# get list of sinks/cards (for settings CARD1/CARD2)
-# pacmd list-sinks | awk '/name:/ {print $0};' | awk '{ print $2}' | sed 's/<//g; s/>//g'
+new_sink=$(pacmd list-sinks | grep index | tee /dev/stdout | grep -m1 -A1 "* index" | tail -1 | cut -c12-)
 
-CARD1="alsa_output.pci-0000_01_00.1.hdmi-stereo-extra5"
-CARD2="alsa_output.usb-C-Media_Electronics_Inc._USB_PnP_Sound_Device-00.iec958-stereo"
+echo "Setting default sink to: $new_sink";
+pacmd set-default-sink $new_sink
+pacmd list-sink-inputs | grep index | while read line
+do
+echo "Moving input: ";
+echo $line | cut -f2 -d' ';
+echo "to sink: $new_sink";
+pacmd move-sink-input `echo $line | cut -f2 -d' '` $new_sink
 
-CURRENT_SINK=$(pacmd stat | awk -F": " '/^Default sink name: /{print $2}' | awk 'BEGIN{FS=OFS="."} NF--' | sed 's/alsa_output/alsa_output/g')
-
-
-setCard() {
-
-  if [ "$CURRENT_SINK" == "$1" ]
-   then
-     echo "Already using this Sink"
-     exit 1
-  fi
-
-  NEW_SINK=$(pacmd list-sinks | awk '/index:/ {print $1 $2 $3} /name:/ {print $0};' | grep -m1 -B1 $1 | grep index | awk '{print $1}' | cut -d ":" -f2)
-  SINK=$(pacmd set-default-sink $NEW_SINK)
-  INPUT=$(pacmd list-sink-inputs | grep index | awk '{print $2}')
-
-  pacmd move-sink-input $INPUT $NEW_SINK
-  echo "Moving input: $INPUT to sink: $NEW_SINK";
-  echo "Setting default sink to: $NEW_SINK";
-
-  notify-send --urgency=normal "Audio Switching" "SINK: $NEW_SINK"
-}
-
-toggleSinks() {
-  if [ "$CURRENT_SINK" == "$CARD1" ]
-    then
-      setCard $CARD2
-    else
-      setCard $CARD1
-    fi
-}
-
-
-showHelp() {
-  echo "------------------------------------"
-  echo "AUDIO SINK SWITCHER"
-  echo " "
-  echo "$0 [options]"
-  echo " "
-  echo "options:"
-  echo "-h  --help        What you are looking at.."
-  echo "-g, --gaming      Sets Gaming headset as output device"
-  echo "-s, --speakers    Sets Speakers as output device"
-  echo "-t, --toggle      Toggles the different output devices"
-  echo " "
-  echo "------------------------------------"
-}
-
-# check args length
-if [ $# -eq 0 ]
-  then
-    echo "Toggling output devices (Speakers/Headset)"
-    toggleSinks
-fi
-
-
-# arg options
-while test $# -gt 0; do
-    case "$1" in
-
-                -h|--help)
-                        showHelp
-                        exit 1
-                        ;;
-
-                -g|--gaming)
-                        setCard $CARD2
-                        exit 1
-                        ;;
-
-                -s|--speakers)
-                        setCard $CARD1
-                        exit 1
-                        ;;
-
-                -t|--toggle)
-                        toggleSinks
-                        echo "Toggling output devices (Speakers/Headset)"
-                        exit 1
-                        ;;
-                 *)
-                        showHelp
-                        exit 1
-                        ;;
-    esac
 done
