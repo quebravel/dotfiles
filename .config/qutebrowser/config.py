542fbb4bee05f3002ce3659f4d51ad806cb8f45d
quebravel <jonatasgb@gmail.com>
2020-05-23 16:35:53 -0300

    mudaça na config.py qutebrowser teclas de atalhos

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -6,17 +6,17 @@
 # Uncomment this to still load settings configured via autoconfig.yml
 # config.load_autoconfig()
 
-c.fonts.tabs = "10pt ctrld"
-c.fonts.statusbar = "10pt ctrld"
-c.fonts.messages.info = "10pt ctrld"
+c.fonts.tabs = "10.5pt tamzenforpowerline"
+c.fonts.statusbar = "10.5pt tamzenforpowerline"
+c.fonts.messages.info = "10.5pt tamzenforpowerline"
 c.fonts.downloads = c.fonts.statusbar
-c.fonts.prompts = "10pt ctrld"
+c.fonts.prompts = "10.5pt tamzenforpowerline"
 c.fonts.keyhint = c.fonts.messages.info
 c.fonts.messages.warning = c.fonts.messages.info
 c.fonts.messages.error = c.fonts.messages.info
-c.fonts.completion.entry = "10pt ctrld"
+c.fonts.completion.entry = "10.5pt tamzenforpowerline"
 c.fonts.completion.category = c.fonts.statusbar
-#c.fonts.hints = "13px monospace"
+#c.fonts.hints = "13px tamzenforpowerline"
 
 # searches
 c.url.searchengines['DEFAULT'] = 'https://www.duckduckgo.com/?q={}'
@@ -40,7 +40,7 @@ c.aliases['gm'] = 'open -t https://mail.google.com'
 c.aliases['fd'] = 'open -t https://feedly.com'
 
 # geral
-#c.new_instance_open_target = "tab-bg"
+c.new_instance_open_target = "tab"
 c.tabs.background = True
 c.downloads.location.directory = '$HOME/Downloads'
 c.spellcheck.languages = ["pt-BR"]
@@ -48,26 +48,23 @@ c.content.headers.accept_language = 'pt-BR,pt'
 c.tabs.show = 'always' #multiple,never,always,switching
 c.url.default_page = "https://start.duckduckgo.com/"
 c.url.start_pages = "https://start.duckduckgo.com/"
-c.hints.chars = "asdfghjklç"
 c.tabs.select_on_remove = "prev"
 c.content.notifications = False
 c.content.autoplay = True
 c.content.geolocation = True
-# https://github.com/alphapapa/solarized-everything-css.git
-#c.content.user_stylesheets = "~/.config/qutebrowser/solarized-everything-css/css/solarized-dark/solarized-dark-all-sites.css"
+c.tabs.mode_on_change = "persist"
+c.tabs.title.format = "{index}: {current_title} {audio}"
+c.colors.webpage.prefers_color_scheme_dark = True
 
 # keys
-config.bind(';d', 'set downloads.location.directory ~/Downloads ;; hint links download')
-config.bind('xx', 'config-cycle statusbar.hide ;; config-cycle tabs.show always switching')
-config.bind('xt', 'config-cycle tabs.show always switching')
-config.bind('xb', 'config-cycle statusbar.hide')
-config.bind('gD', 'spawn urxvt -e ranger /home/jonatas/Downloads')
-
 c.bindings.commands = {
         'normal': {
-            '<tab>': 'nop',
+            #  '<tab>': 'nop',
             'gR': 'spawn urxvt -e ranger /home/jonatas/Downloads',
-            ',n': 'config-cycle content.user_stylesheets  ~/.config/qutebrowser/solarized-everything-css/css/solarized-dark/solarized-dark-all-sites.css "" ;; reload',
+            ',n': 'config-cycle content.user_stylesheets  ~/.config/qutebrowser/solarized-everything-css/css/solarized-dark/solarized-dark-all-sites.css ""',
+            ',d': 'config-cycle content.user_stylesheets ~/.config/qutebrowser/solarized-everything-css/css/apprentice/apprentice-all-sites.css ""',
+            'xx': 'config-cycle statusbar.hide ;; config-cycle tabs.show always switching',
+            'xd': 'set downloads.location.directory ~/Downloads ;; hint links download'
             }
         }
 
