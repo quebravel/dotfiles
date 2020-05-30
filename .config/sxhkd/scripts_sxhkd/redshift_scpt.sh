b951880edc73006620db85003b6c6b9e55ccc2b2
quebravel <jonatasgb@gmail.com>
2020-05-30 19:21:01 -0300

    add redshift script

---
+++ .config/sxhkd/scripts_sxhkd/redshift_scpt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$(pgrep -x redshift)" ]; then
+    pkill redshift &rdunstify -u normal "Redshift" "Desativado"
+else
+    redshift -t 5100:3900 -l -1.396142:-48.386560 &
+    dunstify -u normal "Redshift" "Ativo"
+fi
+
