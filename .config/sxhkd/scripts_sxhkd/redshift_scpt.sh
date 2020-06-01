1fb8c2b65c9675382e87ea5c69813eeb4965a385
quebravel <jonatasgb@gmail.com>
2020-05-31 21:01:00 -0300

    pequeno concerto no redshift script

--- .config/sxhkd/scripts_sxhkd/redshift_scpt.sh
+++ .config/sxhkd/scripts_sxhkd/redshift_scpt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$(pgrep -x redshift)" ]; then
-    pkill redshift &rdunstify -u normal "Redshift" "Desativado"
+    pkill redshift & dunstify -u normal "Redshift" "Desativado"
 else
     redshift -t 5100:3900 -l -1.396142:-48.386560 &
     dunstify -u normal "Redshift" "Ativo"
