80d1a0c80b6324c10b4dc15f8538b98844c4b8ee
quebravel <jonatasgb@gmail.com>
2020-07-04 21:06:49 -0300

    add config britness

--- .config/redshift/hooks/brightness.sh
+++ .config/redshift/hooks/brightness.sh
@@ -9,19 +9,24 @@ brightness_transition="50"
 brightness_night="30"
 # Set fade time for changes to one minute
 fade_time=500
+# Saida
+output_video=DisplayPort-1
 
 case $1 in
 	period-changed)
 		case $3 in
 			night)
-				xbacklight -set $brightness_night -time $fade_time
+                xrandr --output $output_video --brightness 0.8 --gamma 1:1:1
+				# xbacklight -set $brightness_night -time $fade_time
 				;;
 			transition)
-				xbacklight -set $brightness_transition -time $fade_time
+                xrandr --output $output_video --brightness 0.9 --gamma 1:1:1
+				# xbacklight -set $brightness_transition -time $fade_time
 				;;
 			daytime)
-				xbacklight -set $brightness_day -time $fade_time
+                xrandr --output $output_video --brightness 1.0 --gamma 1:1:1
+				# xbacklight -set $brightness_day -time $fade_time
 				;;
 		esac
 		;;
-esac
+e
