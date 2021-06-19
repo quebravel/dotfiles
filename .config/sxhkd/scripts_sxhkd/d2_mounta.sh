1cc0ea0be2e408bc9d3f0dfed50ade367604f73a
quebravel <jonatasgb@gmail.com>
2021-06-18 23:06:26 -0300

    update fix d2_mounta.sh

--- .config/sxhkd/scripts_sxhkd/d2_mounta.sh
+++ .config/sxhkd/scripts_sxhkd/d2_mounta.sh
@@ -3,11 +3,11 @@
 opcao=$1
 
 mounta(){
-    select=`lsblk | grep  "─sd" | cut -c 7-10,22-29 | dmenu -h 45 -y 20 -nb "#0087ff" -nf "#000000" -sb "#fe8019" -sf "#000000" -fn "monaco-10" -p "Selecionar Pendrive"` && dispositivo=`echo $select | cut -c 1-4` && echo -e "Montar\n" | dmenu -mask -noinput -h 45 -y 20 -nb "#0087ff" -nf "#000000" -sb "#fe8019" -sf "#000000" -fn "monaco-10" -p "Montando | Senha" | sudo -S mount -t auto /dev/$dispositivo /mnt && dunstify -u normal -t 2000 "Montando" "/mnt"
+    select=`lsblk | grep  "─sd" | grep " 1 " | cut -c 7-10,22-29 | dmenu -h 18 -nb "#3c3836" -nf "#fbf1c7" -sb "#bdae93" -sf "#1d2021" -fn "Source Code Pro-8" -p "Selecionar Pendrive"` && dispositivo=`echo $select | cut -c 1-4` && echo -e "Montar\n" | dmenu -mask -noinput -h 18 -nb "#3c3836" -nf "#fbf1c7" -sb "#bdae93" -sf "#1d2021" -fn "Source Code Pro-8" -p "Montando | Senha" | sudo -S mount -t auto /dev/$dispositivo /mnt && dunstify -u normal -t 2000 "Montando" "/mnt"
 }
 
 Desmounta(){
-    picom & echo -e "Digite a senha\n" | dmenu -mask -noinput -h 45 -y 20 -nb "#0087ff" -nf "#000000" -sb "#fe8019" -sf "#000000" -fn "monaco-10" -p "Desmontando | Senha" | sudo -S umount -Rf /mnt && dunstify -u normal -t 1500 "Desmontado" "/mnt" ; pkill picom
+    echo -e "Digite a senha\n" | dmenu -mask -noinput -h 18 -nb "#3c3836" -nf "#fbf1c7" -sb "#bdae93" -sf "#1d2021" -fn "Source Code Pro-8" -p "Desmontando | Senha" | sudo -S umount -Rf /mnt && dunstify -u normal -t 1500 "Desmontado" "/mnt"
 }
 
 escolha(){
