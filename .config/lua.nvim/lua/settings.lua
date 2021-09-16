c197b68b8ef435c35ab7e63cdc9c5d8de8527517
quebravel <jonatasgb@gmail.com>
2021-09-15 21:02:04 -0300

    add config nvimtree

--- .config/lua.nvim/lua/settings.lua
+++ .config/lua.nvim/lua/settings.lua
@@ -1,8 +1,9 @@
--- NvimTree
+-- nvimtree
 vim.g['nvim_tree_ignore'] = { '.git', 'node_modules', '.cache' }
 vim.g['nvim_tree_gitignore'] = 1
 vim.g['nvim_tree_auto_close'] = 1
 vim.g['nvim_tree_hide_dotfiles'] = 0
 vim.g['nvim_tree_git_hl'] = 1
 vim.g['nvim_tree_highlight_opened_files'] = 1
-
+vim.g['nvim_tree_show_icons'] = { folder_arrows = 0, git = 1, files = 1, folders = 1 }
+vim.g['nvim_tree_highlight_opened_files'] = 3
