77d8e370fd88ab44506b95ae5c2334741d828abd
quebravel <jonatasgb@gmail.com>
2021-09-19 15:33:17 -0300

    update for dmenu sxhkd help

--- .config/sxhkd/scripts_sxhkd/sxhkd_help.sh
+++ .config/sxhkd/scripts_sxhkd/sxhkd_help.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-awk '/^[a-z]/ && last {print "<small>",$0,"\t",last,"</small>"} {last=""} /^#/{last=$0}' ~/.config/sxhkd/sxhkdrc |
+awk '/^[a-z]/ && last {print "",$0,"\t",last,""} {last=""} /^#/{last=$0}' ~/.config/sxhkd/sxhkdrc |
     column -t -s $'\t' |
-    rofi -no-config -dmenu -i -markup-rows -no-show-icons -width 1000 -lines 15 -yoffset 40 
+    # rofi -no-config -dmenu -i -markup-rows -no-show-icons -width 1000 -lines 15 -yoffset 40 
+    dmenu -l 100 -h 18 -nf "#fbf1c7" -nb "#3c3836" -sf "#1d2021" -sb "#bdae93" -fn "JetBrainsMono Nerd Font-8:normal" -p "Keybindings sxhkd"
