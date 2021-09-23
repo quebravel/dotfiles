d61e160fde7666cd77a6fb7dc12fc80c46b2b6c6
quebravel <jonatasgb@gmail.com>
2021-09-23 19:36:07 -0300

    add + options bufferline

--- .config/lua.nvim/lua/appearence.lua
+++ .config/lua.nvim/lua/appearence.lua
@@ -17,14 +17,14 @@ require('lualine').setup {
 
 -- Color scheme
 vim.cmd[[colorscheme tokyonight]]
-
--- Color scheme
 vim.g.tokyonight_style = "night"
 
 -- configuração para bufferline
 require("bufferline").setup{
       options = {
                   show_buffer_close_icons = false,
+                  close_icon = 'x',
+                  modified_icon = '●',
                   offsets = {
                     {
                       filetype = "NvimTree",
