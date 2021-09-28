e7d46c70c0209878313997fb38a7dae4f4631253
quebravel <jonatasgb@gmail.com>
2021-09-27 21:09:49 -0300

    add cmp-path-calc

--- .config/lua.nvim/lua/plugins.lua
+++ .config/lua.nvim/lua/plugins.lua
@@ -40,7 +40,7 @@ packer.startup(function()
 
   -- use 'hrsh7th/nvim-compe'
 
-  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+  use { 'hrsh7th/nvim-cmp', requires = { 'hrsh7th/cmp-path', 'hrsh7th/cmp-calc' } } -- Autocompletion plugin
   use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
   use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
   use 'L3MON4D3/LuaSnip' -- Snippets plugin
