549d04ad35e7fc89d19277041390a2372f8ef4a0
quebravel <jonatasgb@gmail.com>
2021-09-27 21:23:12 -0300

    remove virtual_text

--- .config/lua.nvim/lua/config-plugins/cmp.lua
+++ .config/lua.nvim/lua/config-plugins/cmp.lua
@@ -50,7 +50,7 @@ cmp.setup {
 }
 
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-  virtual_text = true,
+  virtual_text = false,
   signs = true,
   underline = true,
   update_in_insert = false,
