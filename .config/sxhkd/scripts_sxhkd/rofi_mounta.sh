c53fbb81479b6cc6bf0a171b38328dc202d6bfa5
quebravel <jonatasgb@gmail.com>
2021-10-09 21:55:14 -0300

    update

--- .config/sxhkd/scripts_sxhkd/rofi_mounta.sh
+++ .config/sxhkd/scripts_sxhkd/rofi_mounta.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+if [ -d "~/Pendrive" ]
+then
+   echo "ok"
+else
+   mkdir -p ~/Pendrive
+fi
+
 opcao=$1
 
 mounta(){
-    select=`lsblk | grep  "─sd" | cut -c 7-10,22-29 | rofi -dmenu -config ~/.config/rofi/device.ribbon.rasi -p "Selecionar"` && dispositivo=`echo $select | cut -c 1-4` && echo -e "Montar\n" | rofi -dmenu -config ~/.config/rofi/device.ribbon.rasi -password -p "Digite a senha" | sudo -S mount -t auto /dev/$dispositivo /mnt && dunstify -u normal -t 2000 "Montando" "/mnt"
+    select=`lsblk | grep  "─sd" | grep " 1 " | cut -c 7-10,22-29 | rofi -dmenu -p "Selecionar"` && dispositivo=`echo $select | cut -c 1-4` && echo -e "Montar\n" | rofi -dmenu -password -p "Digite a senha" | sudo -S mount -t auto /dev/$dispositivo ~/Pendrive && dunstify -u normal -t 2000 "Montando" "~/Pendrive"
 }
 
 Desmounta(){
-    echo -e "Digite a senha\n" | rofi -dmenu -config ~/.config/rofi/device.ribbon.rasi -password -p "Desmontando" | sudo -S umount -Rf /mnt && dunstify -u normal -t 1500 "Desmontado" "/mnt"
+    echo -e "Digite a senha\n" | rofi -dmenu -password -p "Desmontando" | sudo -S umount -Rl ~/Pendrive && dunstify -u normal -t 1500 "Desmontado" "~/Pendrive"
 }
 
 escolha(){
@@ -25,3 +32,4 @@ esac
 }
 
 escolha
+
