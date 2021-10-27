bb4cb923c3da81f5230a337118ab939355cce6d5
quebravel <jonatasgb@gmail.com>
2021-10-26 22:09:21 -0300

    update scrip brithness

--- .config/sxhkd/scripts_sxhkd/d2_bright.sh
+++ .config/sxhkd/scripts_sxhkd/d2_bright.sh
@@ -3,12 +3,50 @@
 monitor=`xrandr | grep -w connected | cut -f '1' -d ' '`
 
 nothing=`xrandr --verbose | grep Brightness: | cut -f '1,2' -d '.' | awk '{print $2}'`
+# nothing=`xrandr --verbose | grep Brightness: | tr -cd '0-9 '`
 
-light=`echo -e "1.0\0.90\n0.85\n0.75\n0.65\n0.55\n0.45\n0.35\n0.20\n0.10\n0.05" | dmenu -h 18 -nb "#3c3836" -nf "#fbf1c7" -sb "#bdae93" -sf "#1d2021" -fn "JetBrainsMono Nerd Font-8" -p "Selecionar brilho da tela"`
+# light=`echo -e "1.0\0.90\n0.85\n0.75\n0.65\n0.55\n0.45\n0.35\n0.20\n0.10\n0.05" | dmenu -y 15 -h 18 -nb "#3c3836" -nf "#fbf1c7" -sb "#bdae93" -sf "#1d2021" -fn "JetBrainsMono Nerd Font-8" -p "Selecionar brilho da tela"`
+# 
+# 
+# if [ -z $light ]; then
+#     xrandr --output $monitor --brightness $nothing
+# else
+#     xrandr --output $monitor --brightness $light
+# fi
+# 
+
+incrementa(){
+    vbrlh=`echo $nothing + .10 | bc`
+    # echo 0$vbrlh
+    # echo $vbrlh
+    # echo $nothing
+    if [[ $vbrlh == 1.10 ]]; then
+        exit 0
+    else
+        xrandr --output $monitor --brightness 0$vbrlh
+    fi
+}
+
+decrementa(){
+    vbrlh=`echo $nothing - .10 | bc`
+    # echo $vbrlh
+    # echo $nothing
+    if [[ $vbrlh == .10 ]]; then
+        exit 0
+    else
+        xrandr --output $monitor --brightness 0$vbrlh
+    fi
+}
 
 
-if [ -z $light ]; then
-    xrandr --output $monitor --brightness $nothing
-else
-    xrandr --output $monitor --brightness $light
-fi
+
+while getopts "idh" o; do
+    case "${o}" in
+        i) incrementa
+            ;;
+        d) decrementa
+            ;;
+    esac
+done
+
+
