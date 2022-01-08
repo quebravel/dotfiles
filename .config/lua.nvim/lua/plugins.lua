a511ca9607460744fee2df2b70771f045591df74
Jonatas <jonatasgb@gmail.com>
2022-01-08 15:24:27 -0300

    Update plugins.lua

--- .config/lua.nvim/lua/plugins.lua
+++ .config/lua.nvim/lua/plugins.lua
@@ -1,3 +1,6 @@
+-- fazer o git clone
+-- git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 packer = require 'packer'
 packer.init {
     opt_default = false, -- Default to using opt (as opposed to start) plugins
