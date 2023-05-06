33477c5c62365996dd52ccc07b97a16ba40c2cf3
Jonatas <jonatasgb@gmail.com>
2023-05-06 14:04:04 -0300

    Update config.py

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -54,24 +54,6 @@ c.scrolling.smooth = False
 # instalar adblock com o comando pip install adblock
 #  c.content.blocking.method = "adblock"
 
-# keys
-c.bindings.commands = {
-        'normal': {
-            #  '<tab>': 'nop',
-              'gR': 'spawn urxvt -e ranger /home/jonatas/Downloads'
-            , 'xx': 'config-cycle statusbar.show never always ;; config-cycle tabs.show switching always'
-            , 'xi': 'set downloads.location.directory ~/Imagens ;; hint images download'
-            , ',m': 'hint links spawn mpv --ytdl-format="(bestvideo[height<=720]+bestaudio)[ext=webm]/bestvideo[height<=720]+bestaudio/best[height<=720]/bestvideo+bestaudio/best" --window-scale=1.0 {hint-url}'
-            , ',M': 'spawn mpv --ytdl-format="(bestvideo[height<=720]+bestaudio)[ext=webm]/bestvideo[height<=720]+bestaudio/best[height<=720]/bestvideo+bestaudio/best" --window-scale=1.0 {url}'
-            , ',a': 'hint links userscript youtube_mp3_downloader.sh'
-            , ',A': 'spawn --userscript youtube_mp3_downloader.sh'
-            , ',v': 'hint links userscript youtube_downloader.sh'
-            , ',V': 'spawn --userscript youtube_downloader.sh'
-            , '<': 'fake-key <Shift-\\,>'
-            , '>': 'fake-key <Shift-\\.>'
-            }
-        }
-
 # plugins
 # https://gitlab.com/jgkamat/jblock
 #  import sys, os
@@ -81,3 +63,4 @@ c.bindings.commands = {
 # config.source('solarizedark.py')
 config.source('gruvbox.py')
 config.source('aliases_search.py')
+config.source('bindings_commands.py')
