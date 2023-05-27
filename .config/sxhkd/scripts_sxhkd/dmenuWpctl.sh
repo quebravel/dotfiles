ddf8573975dc9729faef6c1b1a3dc35c947dae66
quebravel <jonatasgb@gmail.com>
2023-05-27 16:43:38 -0300

    update colors

--- .config/sxhkd/scripts_sxhkd/dmenuWpctl.sh
+++ .config/sxhkd/scripts_sxhkd/dmenuWpctl.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-dispositovo_opcoes=$(wpctl status | grep -A 4 "Sinks" | head -n 3 | tail -n 2 | tr -d '│'" " | cut -d"*" -f2 | dmenu)
+dispositovo_opcoes=$(wpctl status | grep -A 4 "Sinks" | head -n 3 | tail -n 2 | tr -d '│'" " | cut -d"*" -f2 | dmenu -y 0 -h 17 -nf "#f8f8f2" -nb "#44475a" -sf "#000" -sb "#bd93f9" -fn "JetBrainsMono Nerd Font-9:normal" -p "pipewire wpctl")
 
 # echo $dispositovo_opcoes
 
 dispositivo_numero=$(echo $dispositovo_opcoes | cut -d. -f1)
 
-echo $dispositivo_numero
+# echo $dispositivo_numero
 
-wpctl set-default $dispositivo_numero
+wpctl set-default $dispositivo_numero &> /dev/null
