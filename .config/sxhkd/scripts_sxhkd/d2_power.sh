c908032f8591449436109168b7b8a0d9a7dc418b
quebravel <jonatasgb@gmail.com>
2023-05-27 23:06:13 -0300

    remove sudo pois agora e controlado pelo polkit

--- .config/sxhkd/scripts_sxhkd/d2_power.sh
+++ .config/sxhkd/scripts_sxhkd/d2_power.sh
@@ -6,15 +6,14 @@
 
 
 
-MENU=`echo -e "Logout\nReboot\nShutdown" | dmenu -h 18 -nb "#3c3836" -nf "#fbf1c7" -sb "#bdae93" -sf "#1d2021" -fn "Source Code Pro-8"`
+MENU=`echo -e "                Shutdown                \n                Reboot                \n                Logout                " | dmenu -h 19 -nb "#fe8019" -nf "#282828" -sb "#282828" -sf "#fbf1c7" -fn "JetBrainsMono Nerd Font-9"`
 
             case "$MENU" in
 
+                *Shutdown) shutdown -P now ;;
+
+                *Reboot) shutdown -r now ;;
 
                 *Logout) bspc wm -r ;;
-
-                *Reboot) sudo shutdown -r now ;;
-
-                *Shutdown) sudo shutdown -P now ;;
 
             esac
