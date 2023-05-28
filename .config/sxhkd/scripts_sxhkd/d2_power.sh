e2037469e135bc211dff3cda6b5b190c43f8eb46
quebravel <jonatasgb@gmail.com>
2023-05-27 23:54:36 -0300

    fix menu power

--- .config/sxhkd/scripts_sxhkd/d2_power.sh
+++ .config/sxhkd/scripts_sxhkd/d2_power.sh
@@ -6,7 +6,7 @@
 
 
 
-MENU=`echo -e "                Shutdown                \n                Reboot                \n                Logout                " | dmenu -h 19 -nb "#fe8019" -nf "#282828" -sb "#282828" -sf "#fbf1c7" -fn "JetBrainsMono Nerd Font-9"`
+MENU=`echo -e "Shutdown\nReboot\nLogout" | dmenu -h 19 -nb "#fe8019" -nf "#282828" -sb "#282828" -sf "#fbf1c7" -fn "JetBrainsMono Nerd Font-9"`
 
             case "$MENU" in
 
