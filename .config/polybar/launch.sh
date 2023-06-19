3621e610f54b1145c342954050dc2e4a11794330
quebravel <jonatasgb@gmail.com>
2023-06-19 00:50:41 -0300

    update laucher polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,10 +4,11 @@
 pkill polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+polybar-msg cmd quit
 
 # Launch bar1
-polybar bspoly &
+polybar bspoly 2>&1 | tee -a /tmp/polybar1.log & disown
 
 # echo "Barra lançada..."
-notify-send --urgency=low "Polybar lançado"
+# notify-send --urgency=low "Polybar lançado"
