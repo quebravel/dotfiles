925ccc95ee0e1cb6ece17a37e6bee6853ab9e9f4
quebravel <jonatasgb@gmail.com>
2023-06-23 19:07:30 -0300

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -60,7 +60,7 @@ config.tab_bar_at_bottom = false
 config.use_fancy_tab_bar = false
 config.show_new_tab_button_in_tab_bar = true
 config.tab_max_width = 25
-config.hide_tab_bar_if_only_one_tab = false
+config.hide_tab_bar_if_only_one_tab = true
 window_decorations = "TITLE | RESIZE"
 
 
