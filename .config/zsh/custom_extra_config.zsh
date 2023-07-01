4547740f3592203b5b8852ab21bc496d4d3d2a22
quebravel <jonatasgb@gmail.com>
2023-07-01 20:26:13 -0300

    update config

--- .config/zsh/custom_extra_config.zsh
+++ .config/zsh/custom_extra_config.zsh
@@ -1,26 +1,27 @@
-export	EDITOR='nvim'
-export	TERMINAL='alacritty'
-export	BROWSER='qutebrowser'
-export	READER='zathura'
-export	SHELL='zsh'
-export	XDG_CURRENT_DESKTOP='hyprland'
+export EDITOR='nvim'
+export TERMINAL='alacritty'
+export BROWSER='qutebrowser'
+export READER='zathura'
+export SHELL='zsh'
+# export	XDG_CURRENT_DESKTOP='hyprland'
 # export  XDG_PICTURES_DIR='/home/jonatas/Imagens/'
 
 [[ ! -f ~/.config/zsh/alias.zsh ]] || source ~/.config/zsh/alias.zsh 
 [[ ! -f ~/.config/zsh/vim-mode.zsh ]] || source ~/.config/zsh/vim-mode.zsh
 # [[ ! -f $HOME/.zsh-vi-mode/zsh-vi-mode.plugin.zsh ]] || source $HOME/.zsh-vi-mode/zsh-vi-mode.plugin.zsh
 
-# autopair zsh
+# Instala autopair se não haver.
 if [[ ! -d ~/.config/zsh/zsh-autopair ]]; then
 	git clone https://github.com/hlissner/zsh-autopair ~/.config/zsh/zsh-autopair
 fi
 
+# Carregar autopair.
 source ~/.config/zsh/zsh-autopair/autopair.zsh
 autopair-init
 
-if [ -z "${WAYLAND_DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
-    dbus-run-session Hyprland
-fi
+# if [ -z "${WAYLAND_DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
+#     dbus-run-session Hyprland
+# fi
 
 # source ~/.config/zsh/zsh-system-clipboard/zsh-system-clipboard.zsh
 
