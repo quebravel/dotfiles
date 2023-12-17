1cd4d594838f7f82554a49727cd2c863be55ff41
Jonatas <jonatasgb@gmail.com>
2023-12-17 09:51:43 -0300

    Update config.py

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -44,10 +44,9 @@ c.tabs.title.format = "{index}: {current_title} {audio}"
 c.colors.webpage.preferred_color_scheme = 'dark'
 c.colors.webpage.darkmode.enabled = True
 c.colors.webpage.darkmode.algorithm = "lightness-cielab"
-c.colors.webpage.darkmode.threshold.text = 150
+c.colors.webpage.darkmode.threshold.foreground = 150
 c.colors.webpage.darkmode.threshold.background = 100
-c.colors.webpage.darkmode.policy.images = 'always'
-c.colors.webpage.darkmode.grayscale.images = 0.35
+c.colors.webpage.darkmode.policy.images = 'smart'
 
 
 # configuracao minha antiga
