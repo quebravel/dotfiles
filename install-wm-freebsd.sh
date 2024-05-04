d4b65094c508f6bde544086ef6e1bf1a321af3d2
Jonatas <jonatasgb@gmail.com>
2024-05-04 19:31:56 -0300

    Update install-wm-freebsd.sh

--- install-wm-freebsd.sh
+++ install-wm-freebsd.sh
@@ -56,7 +56,7 @@ EOL
 
     install_software_pkg "sudo neovim zsh zsh-completions"
 }
-config_zsh(){
+config_ohmyzsh(){
  sleep 0.2
 
 if [[ ! -d ~/.oh-my-zsh ]]; then
@@ -70,6 +70,42 @@ else
 fi
 
 }
-inicio
-config_zsh
 
+alias_autopair(){
+ sleep 0.2
+
+# zsh alias, autopair
+if [[ ! -f ~/.config/zsh/alias.zsh ]]; then
+ mkdir --parents ~/.config/zsh/;
+ cp ./.config/zsh/alias.zsh ~/.config/zsh/;
+ echo -e "$COK - ZSH ALIAS."
+fi
+
+if [[ ! -f ~/.config/zsh/vim-mode.zsh ]]; then
+ mkdir --parents  ~/.config/zsh/;
+ cp ./.config/zsh/vim-mode.zsh ~/.config/zsh/;
+ echo -e "$COK - PLUGIN ZSH VI-MODE."
+fi
+
+if [[ ! -f ~/.config/zsh/custom_extra_config.zsh ]]; then
+ mkdir --parents  ~/.config/zsh/;
+ cp ./.config/zsh/custom_extra_config.zsh ~/.config/zsh/;
+ echo -e "$COK - PLUGIN ZSH EXTRAS."
+fi
+
+sleep 0.2
+
+if [[ -f ~/.zshrc ]]; then
+sed -i '/^\[\[ \!/d' $HOME/.zshrc
+echo "[[ ! -f ~/.config/zsh/custom_extra_config.zsh ]] || source ~/.config/zsh/custom_extra_config.zsh" >> $HOME/.zshrc
+sed -i '/(git)/s/git/git extract universalarchive/' $HOME/.zshrc
+fi
+
+sleep 0.2
+echo -e "$COK - ZSH COM CONFIGURACAO EXTRA."
+
+}
+
+inicio
+config_ohmyzsh
+alias_autopair
