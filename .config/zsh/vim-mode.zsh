6cfcbe4db1cee8945394c71dfd14aa7a7c65473a
Jonatas <jonatasgb@gmail.com>
2024-05-19 19:12:24 -0300

    Update vim-mode.zsh

--- .config/zsh/vim-mode.zsh
+++ .config/zsh/vim-mode.zsh
@@ -29,14 +29,14 @@ bindkey -M menuselect 'j' vi-down-line-or-history
 bindkey -v '^?' backward-delete-char
 
 # Yank to the system clipboard
-function vi-yank-xclip {
-    zle vi-yank
-   # echo "$CUTBUFFER" | xclip -selection "clipboard"
-   echo "$CUTBUFFER" | wl-copy
-}
-
-zle -N vi-yank-xclip
-bindkey -M vicmd 'y' vi-yank-xclip
+# function vi-yank-xclip {
+#     zle vi-yank
+#    # echo "$CUTBUFFER" | xclip -selection "clipboard"
+#    echo "$CUTBUFFER" | wl-copy
+# }
+#
+# zle -N vi-yank-xclip
+# bindkey -M vicmd 'y' vi-yank-xclip
 # End Yank
 
 # Change cursor shape for different vi modes.
@@ -60,3 +60,43 @@ zle-line-init() {
 zle -N zle-line-init
 echo -ne '\e[5 q' # Use beam shape cursor on startup.
 preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
+
+function x11-clip-wrap-widgets() {
+    # NB: Assume we are the first wrapper and that we only wrap native widgets
+    # See zsh-autosuggestions.zsh for a more generic and more robust wrapper
+    local copy_or_paste=$1
+    shift
+
+    for widget in $@; do
+        # Ugh, zsh doesn't have closures
+        if [[ $copy_or_paste == "copy" ]]; then
+            eval "
+            function _x11-clip-wrapped-$widget() {
+                zle .$widget
+                wl-copy <<<\$CUTBUFFER
+            }
+            "
+        else
+            eval "
+            function _x11-clip-wrapped-$widget() {
+                CUTBUFFER=\$(wl-paste)
+                zle .$widget
+            }
+            "
+        fi
+
+        zle -N $widget _x11-clip-wrapped-$widget
+    done
+}
+
+
+local copy_widgets=(
+    vi-yank vi-yank-eol vi-delete vi-backward-kill-word vi-change-whole-line
+)
+local paste_widgets=(
+    vi-put-{before,after}
+)
+
+# NB: can atm. only wrap native widgets
+x11-clip-wrap-widgets copy $copy_widgets
+x11-clip-wrap-widgets paste  $paste_widgets
