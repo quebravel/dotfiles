dcd9dd6d1bb29c31f00a2abec1aa51b70a5e0f64
quebravel <jonatasgb@gmail.com>
2025-10-11 19:18:58 -0300

    add config xbps

---
+++ .config/waybar/niri_waybar-config/scripts/xbps-updates.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pkgs=$(xbps-install -nuM | awk '{print $1}')
+pkg_count=$(echo $pkgs | wc -w)
+pkg_list=$(echo $pkgs | sed 's/ /\\r/g')
+
+echo "{\"text\":\"$pkg_count\", \"tooltip\":\"$pkg_list\"}"
