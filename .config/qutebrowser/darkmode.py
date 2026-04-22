b041144d01967e6998c6c6e57bdbded26732d8e7
quebravel <jonatasgb@gmail.com>
2026-04-21 21:18:06 -0300

    darkmode translate

--- .config/qutebrowser/darkmode.py
+++ .config/qutebrowser/darkmode.py
@@ -1,4 +1,4 @@
-config.set('colors.webpage.darkmode.enabled', False, 'https://www.youtube.com/*')
+# darkmode true
 config.set('colors.webpage.darkmode.enabled', True, 'https://www.dicio.com.br/*')
 config.set('colors.webpage.darkmode.enabled', True, 'qute://settings//*')
 config.set('colors.webpage.darkmode.enabled', True, 'qute://bookmarks/*')
@@ -7,3 +7,8 @@ config.set('colors.webpage.darkmode.enabled', True, 'https://wiki.gentoo.org/*')
 config.set('colors.webpage.darkmode.enabled', True, 'https://archlinux.org/*')
 config.set('colors.webpage.darkmode.enabled', True, 'https://aur.archlinux.org/*')
 config.set('colors.webpage.darkmode.enabled', True, '*://www.edivaldobrito.com.br/*')
+config.set('colors.webpage.darkmode.enabled', True, '*://translate.google.com/*')
+
+# darkmode false
+config.set('colors.webpage.darkmode.enabled', False, '*://keep.google.com/*')
+config.set('colors.webpage.darkmode.enabled', False, 'https://www.youtube.com/*')
